46e998eaa2b60ce635264c4a6f7fe3463c00327a
angelmezapy <angelmezado@gmail.com>
2025-05-24 14:13:19 -0300

    agregar main.py y cliente.json

--- PruebaFinal/main.py
+++ PruebaFinal/main.py
@@ -1,14 +1,14 @@
 from flask import Flask, jsonify, request
 
-app = Flask("__hello__")
+app = Flask("__ci__")
 
 @app.route('/', methods='GET')
-def hello():
-    return 'Hola!'
+def cigit():
+    return 'Nro de cédula'
 
-app.run(host = '201.222.51.207', debug = True, port = 5003)
+app.run(host = '201.222.51.207', debug = True, port = 5000)
 
-app.run(host = '0.0.0.0', debug = True, port = 5003)
+app.run(host = '0.0.0.0', debug = True, port = 5000)
 
 app.run(debug=True)
 
