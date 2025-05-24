84ee37f456a0dfac16b8aff51f78eaf104177381
angelmezapy <angelmezado@gmail.com>
2025-05-24 13:36:04 -0300

    agregar main.py

--- PruebaFinal/main.py
+++ PruebaFinal/main.py
@@ -1,3 +1,14 @@
-pip install flask
+from flask import Flask, jsonify, request
 
-mi_api
+app = Flask(__ci__)
+
+@app.route('/', methods='GET')
+def ci():
+    return 'Hola!'
+
+## app.run (host = '127.0.0.1', debug = true, port = 5003)
+
+app.run(host='0.0.0.0', debug = true, port = 5003)
+
+app.run(debug=true)
+
